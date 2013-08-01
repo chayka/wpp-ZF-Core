7654eac1ff906d5c79b7d8c37bfd796aab54adbb
Boris Mossounov <borix@tut.by>
2013-08-01 20:59:03 +0300

    +QueryModels

--- application/models/taxonomies/TaxonomyQueryModel.php
+++ application/models/taxonomies/TaxonomyQueryModel.php
@@ -1,7 +1,195 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+class TaxonomyQueryModel{
+    protected $vars = array();
+    
+    public function __construct() {
+        ;
+    }
+    
+    public function getVars(){
+        return $this->vars;
+    }
+    
+    /**
+     * 
+     * @param type $key
+     * @param type $value
+     * @return \TermQueryModel
+     */
+    public function setVar($key, $value){
+        $this->vars[$key] = $value;
+        return $this;
+    }
+    
+    /**
+     * Select by name
+     * 
+     * @param string $name
+     * @return \TaxonomyQueryModel
+     */
+    public function name($name){
+        return $this->setVar('name', $name);
+    }
+    
+    /**
+     * Select by post types
+     * 
+     * @param string|array(string) $postTypes
+     * @return \TaxonomyQueryModel
+     */
+    public function postType($postTypes){
+        return $this->setVar('object_type', $name);
+    }
+    
+    /**
+     * Select by label
+     * 
+     * @param string $label
+     * @return \TaxonomyQueryModel
+     */
+    public function label($label){
+        return $this->setVar('label', $label);
+    }
+    
+    /**
+     * Select by label
+     * 
+     * @param string $label
+     * @return \TaxonomyQueryModel
+     */
+    public function labelSingular($label){
+        return $this->setVar('singular_label', $label);
+    }
+    
+    /**
+     * Select by show_ui flag
+     * 
+     * @param boolean $flag
+     * @return \TaxonomyQueryModel
+     */
+    public function showUI($flag){
+        return $this->setVar('show_ui', $flag);
+    }
+    
+    /**
+     * Select by show_tag_cloud flag
+     * 
+     * @param boolean $flag
+     * @return \TaxonomyQueryModel
+     */
+    public function showTagCloud($flag){
+        return $this->setVar('show_tag_cloud', $flag);
+    }
+    
+    /**
+     * Select by public flag
+     * 
+     * @param boolean $flag
+     * @return \TaxonomyQueryModel
+     */
+    public function isPublic($flag){
+        return $this->setVar('public', $flag);
+    }
+    
+    /**
+     * Select by _builtin flag
+     * 
+     * @param boolean $flag
+     * @return \TaxonomyQueryModel
+     */
+    public function isBuiltIn($flag){
+        return $this->setVar('_builtin', $flag);
+    }
+    
+    /**
+     * Select by update_count_callback
+     * 
+     * @param mixed $callback
+     * @return \TaxonomyQueryModel
+     */
+    public function updateCountCallback($callback){
+        return $this->setVar('update_count_callback', $callback);
+    }
+    
+    /**
+     * Select by rewrite
+     * 
+     * @param mixed $rewrite
+     * @return \TaxonomyQueryModel
+     */
+    public function rewrite($rewrite){
+        return $this->setVar('rewrite', $rewrite);
+    }
+    
+    /**
+     * Select by rewrite args
+     * 
+     * @param string $slug Used as pretty permalink text (i.e. /tag/) - defaults to $taxonomy (taxonomy's name slug)
+     * @param boolean $withFront allowing permalinks to be prepended with front base - defaults to true
+     * @param type $hierarchical true or false allow hierarchical urls (implemented in Version 3.1) - defaults to false
+     * @param type $epMask Assign an endpoint mask for this taxonomy - defaults to EP_NONE. For more info see this Make WordPress Plugins summary of endpoints.
+     * @return type
+     */
+    public function rewriteArgs($slug, $withFront = true, $hierarchical = false, $epMask = EP_NONE){
+        return $this->rewrite(array(
+            'slug' => $slug,
+            'with_front' => $withFront,
+            'hierarchical' => $hierarchical,
+            'ep_mask' => $epMask,
+        ));
+    }
+    
+    /**
+     * Select by query_var
+     * 
+     * @param mixed $queryVar
+     * @return \TaxonomyQueryModel
+     */
+    public function queryVar($queryVar){
+        return $this->setVar('rewrite', $queryVar);
+    }
+    
+    /**
+     * Select by manage_cap
+     * 
+     * @param mixed $cap
+     * @return \TaxonomyQueryModel
+     */
+    public function manageCap($cap){
+        return $this->setVar('manage_cap', $cap);
+    }
+    
+    /**
+     * Select by edit_cap
+     * 
+     * @param mixed $cap
+     * @return \TaxonomyQueryModel
+     */
+    public function editCap($cap){
+        return $this->setVar('edit_cap', $cap);
+    }
+    
+    /**
+     * Select by delete_cap
+     * 
+     * @param mixed $cap
+     * @return \TaxonomyQueryModel
+     */
+    public function deleteCap($cap){
+        return $this->setVar('delete_cap', $cap);
+    }
+    
+    /**
+     * Select by assign_cap
+     * 
+     * @param mixed $cap
+     * @return \TaxonomyQueryModel
+     */
+    public function assignCap($cap){
+        return $this->setVar('assign_cap', $cap);
+    }
+    
+    
+    
+}
--- application/models/taxonomies/TermQueryModel.php
+++ application/models/taxonomies/TermQueryModel.php
@@ -1,7 +1,311 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+class TermQueryModel{
+    protected $vars = array();
+    
+    public function __construct() {
+        ;
+    }
+    
+    public function getVars(){
+        return $this->vars;
+    }
+    
+    /**
+     * 
+     * @param type $key
+     * @param type $value
+     * @return \TermQueryModel
+     */
+    public function setVar($key, $value){
+        $this->vars[$key] = $value;
+        return $this;
+    }
+    
+    /**
+     * Designates the ascending or descending order of the 'orderby' parameter. 
+     * Defaults to 'ASC'
+     * @param string $order
+     * @return \TermQueryModel
+     */
+    public function order($order){
+        return $this->setVar('order', $order);
+    }
+    
+    public function order_ASC(){
+        return $this->order('ASC');
+    }
+   
+    public function order_DESC(){
+        return $this->order('DESC');
+    }
+    
+    /**
+     * Sort retrieved users by parameter. Defaults to 'login'.
+     * 
+     * @param string $orderBy
+     * @return \TermQueryModel
+     */
+    public function orderBy($orderBy){
+        return $this->setVar('orderby', $orderBy);
+    }
+    
+    public function orderBy_ID(){
+        return $this->orderBy('id');
+    }
+    
+    public function orderBy_Count(){
+        return $this->orderBy('count');
+    }
+
+    public function orderBy_Name(){
+        return $this->orderBy('name');
+    }
+    
+    public function orderBy_Slug(){
+        return $this->orderBy('slug');
+    }
+    
+    public function orderBy_TermGroup(){
+        return $this->orderBy('term_group');
+    }
+    
+    public function orderBy_None(){
+        return $this->orderBy('none');
+    }
+    
+    /**
+     * Whether to return empty $terms
+     * @param boolean $hide
+     * @return \TermQueryModel
+     */
+    public function hideEmpty($hide = true){
+        return $this->setVar('hide_empty', $hide);
+    }
+    
+    /**
+     * Whether to return empty $terms
+     * @param boolean $show
+     * @return \TermQueryModel
+     */
+    public function showEmpty($show = true){
+        return $this->hideEmpty(!$show);
+    }
+    
+    /**
+     * An array of term ids to exclude. Also accepts a string of comma-separated ids.
+     * 
+     * @param integer|string|array $termIds
+     * @return \TermQueryModel
+     */
+    public function excludeIds($termIds){
+        return $this->setVar('exclude', $termIds);
+    }
+    
+    /**
+     * An array of parent term ids to exclude
+     * 
+     * @param integer|string|array $parentTermIds
+     * @return \TermQueryModel
+     */
+    public function excludeTreeIds($parentTermIds){
+        return $this->setVar('exclude_tree', $parentTermIds);
+    }
+    
+    /**
+     * An array of term ids to include. Empty returns all.
+     * 
+     * @param integer|string|array $termIds
+     * @return \TermQueryModel
+     */
+    public function includeIds($termIds){
+        return $this->setVar('include', $termIds);
+    }
+    
+    /**
+     * The maximum number of terms to return. Default is to return them all.
+     * 
+     * @param int $number
+     * @return \TermQueryModel
+     */
+    public function number($number){
+        return $this->setVar('number', $number);
+    }
+    
+    /**
+     * Set return values.
+     * 
+     * @param string|array(string) $fields
+     * @return \TermQueryModel
+     */
+    public function fields($fields){
+        return $this->setVar('fields', $fields);
+    }
+    
+    /**
+     * all - returns an array of term objects - Default
+     * 
+     * @return \TermQueryModel
+     */
+    public function fields_All(){
+        return $this->fields('all');
+    }
+    
+    /**
+     * ids - returns an array of integers
+     * 
+     * @return \TermQueryModel
+     */
+    public function fields_Ids(){
+        return $this->fields('ids');
+    }
+    
+    /**
+     * names - returns an array of strings
+     * 
+     * @return \TermQueryModel
+     */
+    public function fields_Names(){
+        return $this->fields('names');
+    }
+    
+    /**
+     * count - (3.2+) returns the number of terms found
+     * 
+     * @return \TermQueryModel
+     */
+    public function fields_Count(){
+        return $this->fields('count');
+    }
+    
+    /**
+     * id=>parent - returns an associative array where 
+     * the key is the term id and 
+     * the value is the parent term id if present or 0
+     * 
+     * @return \TermQueryModel
+     */
+    public function fields_ID_ParentId(){
+        return $this->fields('id=>parent');
+    }
+    
+    /**
+     * Returns terms whose "slug" matches this value. Default is empty string.
+     * 
+     * @param string $slug
+     * @return \TermQueryModel
+     */
+    public function slug($slug){
+        return $this->setVar('slug', $slug);
+    }
+    
+    /**
+     * Get direct children of this term (only terms whose explicit parent is this value). 
+     * If 0 is passed, only top-level terms are returned. Default is an empty string.
+     * 
+     * @param int $parentTermId
+     * @return \TermQueryModel
+     */
+    public function parentId($parentTermId){
+        return $this->setVar('parent', $parentTermId);
+    }
+    
+    /**
+     * Whether to include terms that have non-empty descendants 
+     * (even if 'hide_empty' is set to true).
+     * 
+     * @param type $hierarchical
+     * @return \TermQueryModel
+     */
+    public function hierarchical($hierarchical){
+        return $this->setVar('hierarchical', $hierarchical);
+    }
+    
+    public function hierarchical_Yes(){
+        return $this->hierarchical(true);
+    }
+    
+    public function hierarchical_No(){
+        return $this->hierarchical(false);
+    }
+    
+    /**
+     * Get all descendents of this term. Default is 0.
+     * 
+     * @param int $parentTermId
+     * @return \TermQueryModel
+     */
+    public function childOf($parentTermId){
+        return $this->setVar('child_of', $parentTermId);
+    }
+    
+    /**
+     * Default is nothing . Allow for overwriting 'hide_empty' and 'child_of', 
+     * which can be done by setting the value to 'all'.
+     * 
+     * @param string $value
+     * @return \TermQueryModel
+     */
+    public function get($value){
+        return $this->setVar('get', $value);
+    }
+    
+    /**
+     * The term name you wish to match. It does a LIKE 'term_name%' query. 
+     * This matches terms that begin with the 
+     * 
+     * @param string $name
+     * @return \TermQueryModel
+     */
+    public function nameLike($name){
+        return $this->setVar('name__like', $name);
+    }
+    
+    /**
+     * If true, count all of the children along with the $terms.
+     * 
+     * @param boolean $count
+     * @return \TermQueryModel
+     */
+    public function padCounts($count = true){
+        return $this->setVar('pad_counts', $count);
+    }
+    
+    /**
+     * The number by which to offset the terms query.
+     *  
+     * @param int $offset
+     * @return \TermQueryModel
+     */
+    public function offset($offset){
+        return $this->setVar('offset', $offset);
+    }
+    
+    /**
+     * The term name you wish to match. It does a LIKE '%term_name%' query. 
+     * This matches terms that contain the 'search'  
+     * 
+     * @param string $name
+     * @return \TermQueryModel
+     */
+    public function search($name){
+        return $this->setVar('search', $name);
+    }
+    
+    /**
+     * Version 3.2 and above. The 'cache_domain' argument enables a unique cache key 
+     * to be produced when the query produced by get_terms() is stored in object cache. 
+     * For instance, if you are using one of this function's filters to modify the query 
+     * (such as 'terms_clauses'), setting 'cache_domain' to a unique value will not 
+     * overwrite the cache for similar queries. Default value is 'core'.
+     * 
+     * @param string $domain
+     * @return \TermQueryModel
+     */
+    public function cacheDomain($domain = 'core'){
+        return $this->setVar('cache_domain', $domain);
+    }
+    
+    
+}
+    

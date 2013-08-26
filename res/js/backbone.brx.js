6ce544d1096c91e6d49151091a959df837e7fe2b
Boris Mossounov <borix@tut.by>
2013-08-26 14:34:41 +0300

    CheckPassword

--- res/js/backbone.brx.js
+++ res/js/backbone.brx.js
@@ -1009,15 +1009,15 @@
             return this.checkRegexp( fieldName, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, errorMessage );
         },
 		
-        checkPassword: function (fieldId, min, errorTemplate){
+        checkPassword: function (fieldName, min, errorTemplate){
             min = min || this.fields(fieldName).attr('check-pass-min') || 0;
             errorTemplate = errorTemplate 
                 || this.fields(fieldName).attr('check-pass-message')
                 || "Пароль должен быть не короче <%= min %> символов."
             var errorMessage = _.template(errorTemplate, {min: min});
-            var input = this.options.inputs[fieldId];
+            var input = this.options.inputs[fieldName];
             if ( input.val().length < min ) {
-                this.setFormFieldStateError(fieldId, errorMessage);
+                this.setFormFieldStateError(fieldName, errorMessage);
                 return false;
             } else {
                 return true;

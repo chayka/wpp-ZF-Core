4b1faf0dd3a99be39dd92543a237ba33b47474e9
Boris Mossounov <borix@tut.by>
2013-08-26 19:43:51 +0300

    Social likes

--- application/controllers/SocialController.php
+++ application/controllers/SocialController.php
@@ -7,6 +7,11 @@ class ZFCore_SocialController extends Zend_Controller_Action{
     }
 
     public function fbChannelAction(){
-        die('<script src="//connect.facebook.net/en_US/all.js"></script>');
+        $locale = InputHelper::getParam('locale', 'en_US');
+        $cache_expire = 60*60*24*365;
+        header("Pragma: public");
+        header("Cache-Control: max-age=".$cache_expire);
+        header('Expires: ' . gmdate('D, d M Y H:i:s', time()+$cache_expire) . ' GMT');
+        die('<script src="//connect.facebook.net/'.$locale.'/all.js"></script>');
     }
 }
--- application/helpers/SocialHelper.php
+++ application/helpers/SocialHelper.php
@@ -15,13 +15,23 @@ class SocialHelper {
     /**
      * https://developers.facebook.com/docs/facebook-login/getting-started-web/
      */
-    public static function fbInit($appId = 'YOUR_APP_ID'){
+    public static function fbInit($appId = 'YOUR_APP_ID', $locale = 'en_US'){
         $html = new Zend_View();
         $html->setScriptPath(ZF_CORE_APPLICATION_PATH.'/views/scripts/social/');
 //        print_r($params);
         $html->appId = $appId;
+        $html->locale = $locale;
         echo $html->render('fb-init.phtml');
     }
+
+    public static function linkedInInit($locale = 'en_US'){
+        $html = new Zend_View();
+        $html->setScriptPath(ZF_CORE_APPLICATION_PATH.'/views/scripts/social/');
+//        print_r($params);
+        $html->locale = $locale;
+        echo $html->render('linked-in-init.phtml');
+    }
+
 }
 
 

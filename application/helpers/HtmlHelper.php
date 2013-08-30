6d1c1cea4f730d7cbf1a01b4b76d2bb7fa819122
Boris Mossounov <borix@tut.by>
2013-08-30 20:21:52 +0300

    Fun with seo

--- application/helpers/HtmlHelper.php
+++ application/helpers/HtmlHelper.php
@@ -11,7 +11,66 @@
  * @author borismossounov
  */
 class HtmlHelper {
+    
+    protected static $meta = array();
 
+    public static function setMeta($key, $value){
+        self::$meta[$key]=$value; 
+    }
+    
+    public static function getMeta($key, $default = ''){
+        return Util::getItem(self::$meta, $key, $default);
+    }
+    
+    public static function setHeadTitle($title){
+        return self::setMeta('head.title', $title);
+    }
+    
+    public static function getHeadTitle($default = ''){
+        return self::getMeta('head.title', $default);
+    }
+    
+    public static function setMetaKeywords($value){
+        return self::setMeta('meta.keywords', $value);
+    }
+    
+    public static function getMetaKeywords($default){
+        return self::getMeta('meta.keywords', $default);
+    }
+    
+    public static function setMetaDescription($value){
+        return self::setMeta('meta.description', $value);
+    }
+    
+    public static function getMetaDescription($default = ''){
+        return self::getMeta('meta.description', $default);
+    }
+    
+    /**
+     * 
+     * @param int|object|PostModel $post
+     */
+    public static function setPostMeta($post){
+        if(is_object($post)){
+            if(!($post instanceof PostModel)){
+                $post = PostModel::unpackDbRecord($post);
+            }
+        }else{
+            $post = PostModel::selectById($post);
+        }
+        
+        self::setHeadTitle($post->getTitle());
+        self::setMetaDescription($post->getExcerpt());
+        $terms = $post->loadTerms();
+        $keywords = array();
+        if($terms){
+            foreach($terms as $taxonomy=>$ts){
+                $keywords = array_merge($keywords, $ts);
+            }
+        }
+        self::setMetaKeywords(join(', ', $keywords));
+    }
+    
     public static function hidden($condition = true){
         if($condition){
             echo 'style="display: none;"';

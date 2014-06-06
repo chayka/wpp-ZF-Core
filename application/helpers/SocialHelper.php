df3c869b4491cf855f9bc1efe7f3dc8d374d4ac9
Boris Mossounov <borix@tut.by>
2014-06-06 15:58:23 +0300

    Social login tests

--- application/helpers/SocialHelper.php
+++ application/helpers/SocialHelper.php
@@ -15,12 +15,13 @@ class SocialHelper {
     /**
      * https://developers.facebook.com/docs/facebook-login/getting-started-web/
      */
-    public static function fbInit($appId = 'YOUR_APP_ID', $locale = 'en_US'){
+    public static function fbInit($appId = 'YOUR_APP_ID', $locale = 'en_US', $fbLoginJsCallback = null){
         $html = new Zend_View();
         $html->setScriptPath(ZF_CORE_APPLICATION_PATH.'/views/scripts/social/');
 //        print_r($params);
         $html->appId = $appId;
         $html->locale = $locale;
+        $html->callback = $fbLoginJsCallback;
         echo $html->render('fb-init.phtml');
     }
 

a0fb22884b2d8930daa3e97dbfe23fa6724d7909
Boris Mossounov <borix@tut.by>
2017-04-22 14:28:58 +0300

    Deprecated error_reporting

--- ZF-Query.php
+++ ZF-Query.php
@@ -128,8 +128,7 @@ class ZF_Query extends WP_Query {
             }
             $request->query_vars['pagename']='zf';
             ini_set('display_errors', 1);
-            error_reporting(E_ALL);
-
+            error_reporting(E_ALL ^ E_DEPRECATED);
             $args = array(
                 'public' => false,
                 'publicly_queryable' => false,
